64a61028f5bcf271fa781eb8620dc2806757f0b6
sand9985 <sand9985@gmail.com>
2014-10-10 09:42:04 +0800

    修改

--- my_controls/Number_Textbox.cs
+++ my_controls/Number_Textbox.cs
@@ -2,8 +2,7 @@
 #===================================
 #
 #  Number_Textbox ver 1.0
-#  作者:NULL(w100386435)
-#  個人小屋:http://home.gamer.com.tw/homeindex.php?owner=w100386435
+#  作者:sand9985
 #  轉載請保留此標籤
 #====================================
 */
@@ -16,29 +15,22 @@ using System.Windows.Forms;
 
  class Number_Textbox:TextBox
     {
-      private int min;
-      private int max;
-
+   
      public Number_Textbox():base()
      {
-         //預設
-         min = 0;
-         max = 100;
+       
 
      }
 
-     //設定修正範圍
-     public void set_range(int min,int max){
-         this.min = min;
-         this.max = max;
+  
 
-
-     }
 
       //只能輸入數字
      protected override void OnKeyPress(KeyPressEventArgs e)
        {
             base.OnKeyPress(e);
+            if (e.KeyChar == '-' && this.Text.Length==0) return;
+
             if (!char.IsDigit(e.KeyChar) && !char.IsControl(e.KeyChar))
             {
                 e.Handled = true;
