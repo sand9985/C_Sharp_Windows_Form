54a5d6400cd513bd60d0deb57e0955815612dbc5
sand9985 <sand9985@gmail.com>
2014-10-18 15:44:36 +0800

    修改Mainmenu

--- my_controls/MainMenu.cs
+++ my_controls/MainMenu.cs
@@ -3,7 +3,8 @@
 #===================================
 #
 #  Main menu example g ver 1.0
-#  作者:sand9985
+#  作者:NULL(w100386435)
+#  個人小屋:http://home.gamer.com.tw/homeindex.php?owner=w100386435
 #  轉載請保留此標籤
 #====================================
 */
@@ -17,7 +18,7 @@ using System.Windows.Forms;
     class MainMenu : MenuStrip
     {
         //0:檔案 1:影像 2:說明
-        public ToolStripMenuItem[] m_items; //項目表
+        public Dictionary<String,ToolStripMenuItem> m_items; //項目表
        
 
         public MainMenu():base()
@@ -27,36 +28,51 @@ using System.Windows.Forms;
          //創建menu
         private void create_menu()
         {
-             m_items = new ToolStripMenuItem[8];  
-            int id=0;
+            m_items = new Dictionary<String, ToolStripMenuItem>();
+            ToolStripMenuItem temp=null;
             string[] main_item = new string[] { "檔案", "影像", "說明" }; //主要的item
            
-            for (int i = 0; i < main_item.Length; i++,id++)
+            for (int i = 0; i < main_item.Length; i++)
             {
-                m_items[id] = new ToolStripMenuItem(main_item[i]);
-                this.Items.Add(m_items[id]);
+                temp = new ToolStripMenuItem(main_item[i]);
+                this.m_items.Add(main_item[i], temp);
+                this.Items.Add(temp);
+
 
             }
             //0:檔案下拉式選單
-            m_items[id] = new ToolStripMenuItem("開啟舊檔");
-            m_items[0].DropDownItems.Add(m_items[id]); id++;
 
-            m_items[id] = new ToolStripMenuItem("儲存檔案");
-            m_items[0].DropDownItems.Add(m_items[id]); id++;
-            m_items[id] = new ToolStripMenuItem("另存新檔");
-            m_items[0].DropDownItems.Add(m_items[id]); id++;
-            m_items[id] = new ToolStripMenuItem("離開");
-            m_items[0].DropDownItems.Add(m_items[id]); id++; 
+            temp = new ToolStripMenuItem("開啟舊檔");
+            this.m_items.Add("開啟舊檔", temp);
+            this.m_items["檔案"].DropDownItems.Add(temp);
+            temp = new ToolStripMenuItem("儲存檔案");
+            this.m_items.Add("儲存檔案", temp);
+            this.m_items["檔案"].DropDownItems.Add(temp);
+            temp = new ToolStripMenuItem("另存新檔");
+            this.m_items.Add("另存新檔", temp);
+            this.m_items["檔案"].DropDownItems.Add(temp);
+
+            temp = new ToolStripMenuItem("離開");
+            this.m_items.Add("離開", temp);
+            this.m_items["檔案"].DropDownItems.Add(temp);
+
+
             //current id =7
+            //1:影像 下拉式選單
+
+            temp = new ToolStripMenuItem("灰階");
+            this.m_items.Add("灰階", temp);
+            this.m_items["影像"].DropDownItems.Add(temp);   
 
 
             //2:說明 下拉式選單
-            m_items[id] = new ToolStripMenuItem("關於作者");
-            
-            m_items[2].DropDownItems.Add(m_items[id]); id++; 
+            temp = new ToolStripMenuItem("關於作者");
+            this.m_items.Add("關於作者", temp);
+            this.m_items["說明"].DropDownItems.Add(temp);   
+ 
              
 
-            //Click為指派按下事件
+         
 
         }
         
